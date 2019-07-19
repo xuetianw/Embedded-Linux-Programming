a34a3cb55aa1377e85e906713bc6c35013707035
xuetianw <xuetianw@sfu.ca>
2019-07-18 20:15:58 -0700

    add asd.h

---
+++ a2d.h
@@ -0,0 +1,11 @@
+//
+// Created by fred on 18/07/19.
+//
+
+#ifndef CMPT433ASS2_A2D_H
+#define CMPT433ASS2_A2D_H
+
+void A2D_init();
+void pot_start_reading ();
+void por_cleanup();
+#endif //CMPT433ASS2_A2D_H
