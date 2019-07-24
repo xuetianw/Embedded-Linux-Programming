594b076163981b3e9c8c8d3227994e0314fb558c
xuetianw <xuetianw@sfu.ca>
2019-07-24 14:13:22 -0700

    update output directory in cmake

--- main.c
+++ main.c
@@ -4,12 +4,18 @@
 
 int main() {
 
+//    A2D_init();
 
-
+//    pot_start_reading();
     UdpListener_startListening();
 
     printf("Hello, World!\n");
 
     Udp_cleanup();
+//    por_cleanup();
+
+    static unsigned long long int *s_pPrimes = NULL;
+    unsigned long long int *pNewArray = malloc(sizeof(*pNewArray) * 2);
+
     return 0;
 }
